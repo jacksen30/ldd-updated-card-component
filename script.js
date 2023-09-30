123fd1ccae60cb13d9ccfcd95b76c86ca0ccc939
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-09-30 22:28:12 +0800

    Add event listener and flip - toggle function for deal card

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript to toggle card rotation on click
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+  const toggleButtons = card.querySelectorAll(".toggle-button");
+
+  toggleButtons.forEach(button => {
+    button.addEventListener("click", function() {
+      card.classList.toggle("is-flipped");
+    });
+  });
+});
