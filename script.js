83bf5030b56a2d3bd4638c24fdee9e6f76198094
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-10-08 18:57:57 +0800

    javascript card flip function returned to original state

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 // JavaScript to toggle card rotation on click
 
-// const cards = document.querySelectorAll(".card");
-
-// cards.forEach(card => {
-//   const toggleButtons = card.querySelectorAll(".toggle-button");
-
-//   toggleButtons.forEach(button => {
-//     button.addEventListener("click", function() {
-//       card.classList.toggle("is-flipped");
-//     });
-//   });
-// });
-
 const cards = document.querySelectorAll(".card");
 
 cards.forEach(card => {
@@ -19,14 +7,7 @@ cards.forEach(card => {
 
   toggleButtons.forEach(button => {
     button.addEventListener("click", function() {
-      alert("Button clicked: " + button.className);  // Alert when button is clicked
       card.classList.toggle("is-flipped");
-      // Check if class was toggled
-      if (card.classList.contains("is-flipped")) {
-        alert("Card is flipped");
-      } else {
-        alert("Card is not flipped");
-      }
     });
   });
 });
